af6217c4c642c1ef326fe55cb3d4b1189f38cd59
Zowie Erickson <zowie.erickson@gmail.com>
2021-05-14 18:25:49 -0400

    added javascript interactivity with up arro

--- js/app.js
+++ js/app.js
@@ -1,5 +1,19 @@
 const scrollButton = document.getElementById("js-top");
 console.log(scrollButton);
 
+const scrollFunc = () => {
+  // Get the current scroll value
+  let y = window.scrollY;
+
+  // If the scroll value is greater than the window height, add a class to show it
+  if (y > 0) {
+    scrollButton.className = "top-link show";
+  } else {
+    scrollButton.className = "top-link hide";
+  }
+};
+
+window.addEventListener("scroll", scrollFunc);
+
 scrollButton.style.textDecoration = "none";
 scrollButton.style.color = "#FFF";
