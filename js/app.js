e82bbf54fe7b6a90914d545e39cdb3edc03bdd4e
Zowie Erickson <zowie.erickson@gmail.com>
2021-05-14 13:24:23 -0400

    fixed margin/padding in about section

--- js/app.js
+++ js/app.js
@@ -1,17 +1,5 @@
 const scrollButton = document.getElementById("js-top");
-const scrollFunc = () => {
-  // Get the current scroll value
-  let y = window.scrollY;
-
-  // If the scroll value is greater than the window height, add a class to show it
-  if (y > 0) {
-    scrollButton.className = "top-link show";
-  } else {
-    scrollButton.className = "top-link hide";
-  }
-};
-
-window.addEventListener("scroll", scrollFunc);
+console.log(scrollButton);
 
 scrollButton.style.textDecoration = "none";
 scrollButton.style.color = "#FFF";
